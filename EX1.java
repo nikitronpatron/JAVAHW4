d5ac94357d7e2b7cdde02e060c463c5b3f45cfe2
nikitronpatron <nikita.borsh9876@gmail.com>
2023-02-27 19:43:00 +0300

    Test

---
+++ EX1.java
@@ -0,0 +1,53 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.LinkedList;
+public class EX1
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Введите строку или print: ");
+        String data = input.next();
+        String[] dat = data.split("~");
+        LinkedList<String> list = new LinkedList<>();
+        String str1 = new String("Nikitron");
+        String str2 = new String("Timur");
+        String str3 = new String("Egor");
+        String str4 = new String("Artyom");
+        list.add(str1);
+        list.add(str2);
+        list.add(str3);
+        list.add(str4);
+        if(dat[0] == "print")
+        {
+            System.out.printf("Полный список:\n%s\nВыбранный элемент:", list);
+            int tmp = Integer.parseInt(dat[1]);
+            list.get(tmp - 1);
+        }
+        else
+        {
+            int tmp = Integer.parseInt(dat[1]);
+            list.add(tmp - 1, dat[0]);
+            System.out.println(list);
+        }
+        
+        
+        //System.out.print("~");
+        //String num = input.next();
+        //String[] dat = data.split("~");
+        //LinkedList<String> list = new LinkedList<>();
+        // for(String word : dat)
+        // {
+        //     if(isDigit(word))
+        //     {
+        //         if(word == "print")
+        //         {
+                
+        //             System.out.println();
+        //         }
+        //     }
+                
+            
+        // }
+    }
+}
